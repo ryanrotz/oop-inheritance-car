e4fb6fddbe4d8107f7b5bd3f1a2db5d864756620
Ryan Rotz <ryanrotz@gmail.com>
2016-06-10 16:54:05 -0700

    oop inheritance car Complete

--- src/Truck.js
+++ src/Truck.js
@@ -1,6 +1,17 @@
 var Car = require('./Car.js');
 
 // Implement your Truck here
-function Truck() {}
+function Truck(make, model, year, color, passengers) {
+  Car.call(this, make, model, year, color, 3, passengers);
+  // this.make = 'Chevy';
+  // this.model = 'Silverado';
+  // this.year = 2016;
+  // this.color = 'black';
+  // this.seats = 3;
+  // this.passengers = ['Hank Hill'];
+}
+
+Truck.prototype = Object.create(Car.prototype);
+Truck.prototype.constructor = Truck;
 
 module.exports = Truck;
--- test/motorcycleTest.js
+++ test/motorcycleTest.js
@@ -7,3 +7,52 @@ var Motorcycle = require('../src/Motorcycle');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+var motorcycle = new Motorcycle('Triumph', 'Rocket III Roadster', 2016, 'white', ['Johnny Rocket']);
+
+console.log('Testing if motorcycle is an instanceof Motorcycle');
+assert.strictEqual(motorcycle instanceof Motorcycle, true, "motorcycle should be an instance of Motorcycle");
+success();
+
+console.log('Testing if motorcycle is an instanceof Car');
+assert.strictEqual(motorcycle instanceof Car, true, "motorcycle should be an instance of Car");
+success();
+
+console.log('Testing if motorcycle has make property');
+assert.strictEqual(motorcycle.make, 'Triumph', "motorcycle should have a make property");
+success();
+
+console.log('Testing if motorcycle has model property');
+assert.strictEqual(motorcycle.model, 'Rocket III Roadster', "motorcycle should have a model property");
+success();
+
+console.log('Testing if motorcycle has year property');
+assert.strictEqual(motorcycle.year, 2016, "motorcycle should have a year property");
+success();
+
+console.log('Testing if motorcycle has color property');
+assert.strictEqual(motorcycle.color, 'white', "motorcycle should have a color property");
+success();
+
+console.log('Testing if motorcycle has 2 seats');
+assert.strictEqual(motorcycle.seats, 2, "motorcycle should have 3 seats");
+success();
+
+console.log('Testing if motorcycle has model passengers');
+assert.deepEqual(motorcycle.passengers, ['Johnny Rocket'], "motorcycle should have passengers");
+success();
+
+// Wheelie function tests
+
+console.log('testing if motorcycle does a wheelie when not running');
+assert.strictEqual(motorcycle.wheelie(), false, "motorcycle should not perform a wheelie when not running");
+success();
+
+console.log('testing if motorcycle does a wheelie when running');
+motorcycle.start();
+assert.strictEqual(motorcycle.wheelie(), true, "motorcycle should not perform a wheelie when not running");
+success();
+
+console.log('Testing if wheelie function is attached to Car.prototype');
+assert.strictEqual(typeof Car.prototype.wheelie === 'function', true, 'wheelie function should be attached to Car.prototype');
+success();
+
